926aad06502238c1dcaa1338f3900d585895fae9
AgustinM12 <agustinmazza12345@gmail.com>
2024-04-19 10:33:39 -0300

    FIX: se modifico la funcion para que funcione con listas

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
 import pandas as pd
 
-# Leer el archivo CSV original, ignorando la primera fila que contiene los nombres de las columnas
-data_frame = pd.read_csv("edades-30Alumnos.csv", delimiter=",", header=None)
+lista_cargada = [21, 29, 19, 22, 23, 19, 30, 19, 19, 19, 20, 20, 20, 18, 22, 19, 34, 34, 21, 21, 22, 28, 29, 19, 20, 19, 25, 28, 21, 22]
 
-# Definir la función para el análisis estadístico
-def analisis_estadistico(df):
+# * Definir la función para el análisis estadístico
+def analisis_estadistico(list, name):
     try:
-        # Verificar si el DataFrame está vacío
-        if df.empty:
-            raise Exception("El DataFrame está vacío")
+        # * Verificar si la lista está vacía
+        if list == []:
+            raise Exception("El primer parametro no es una lista o esta vacia")
         
-        # Verificar si la columna de edades contiene valores no numéricos
-        for i in df.iloc[:, 1]:
+        # * Verificar si la columna de edades contiene valores no numéricos
+        for i in list:
             if not str(i).isnumeric():
                 raise Exception("La columna de edades contiene valores no numéricos")
-        
-        # Crear un DataFrame vacío para almacenar los resultados del análisis
+
+        # * Crear una Serie con los valores de la columna de edades
+        serie = pd.DataFrame(list, columns=[name])
+
+        # * Crear un DataFrame vacío para almacenar los resultados del análisis
         analisis = pd.DataFrame()
         
-        # Crear las columnas del DataFrame de resultados del analisis estadistico
-        analisis["edades"] = df.iloc[:, 1].value_counts().index
-        analisis["fi"] = df.iloc[:, 1].value_counts().values
+        # * Crear las columnas del DataFrame de resultados del analisis estadistico
+        analisis[name] = serie.iloc[:, 0].value_counts().index
+        analisis["fi"] = serie.iloc[:, 0].value_counts().values
         analisis["Fi"] = analisis["fi"].cumsum()
         analisis["ri"] = analisis["fi"] / analisis["fi"].sum()
         analisis["Ri"] = analisis["ri"].cumsum()
@@ -28,9 +30,11 @@ def analisis_estadistico(df):
         analisis["Pi%"] = analisis["Ri"] * 100
         
         return analisis
-    # Manejar excepciones en caso de que haya valores no numericos u otros errores en general
+    # * Manejar excepciones en caso de que haya valores no numericos u otros errores en general
     except Exception as e:
         return e
 
-# Llamar a la función con el DataFrame que contiene los datos originales
-print(analisis_estadistico(data_frame))
+# * Llamar a la función que realiza el análisis estadístico y mostrarlo
+analisis = analisis_estadistico(lista_cargada, "edades")
+print(type(analisis))
+print(analisis)
